c69e141af6945b3d9a8ccf55fdb5c6a6c55d4c14
DivideByZero404 <sreenithi028@gmail.com>
2025-08-26 19:30:35 +0530

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+#fizzbuzz 
+
+def func(n):
+    for i in range(1,n+1):
+        if i % 3 == 0 and i % 5 == 0:
+         print("fizzbuzz")
+        elif i%3==0:
+            print("fizz")
+        elif i%5==0:
+            print("buzz")
+        else:
+            print(i)
+
+func(15)
